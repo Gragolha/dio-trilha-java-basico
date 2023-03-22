ab6d6ffb230b1836ddf377b1295c0eb7c8dfe131
Gragolha <luka.100.nerozi@gmail.com>
2023-03-22 20:30:14 -0300

    second commit study

---
+++ src/novoTeste/operadores/Operadores001.java
@@ -0,0 +1,37 @@
+package novoTeste.operadores;
+
+import java.util.Date;
+
+public class Operadores001 {
+
+	public static void main(String[] args) {
+		
+		String nome = "Roger";
+		int idade = 22;
+		double peso = 68.5;
+		char sexo = 'M';
+		boolean doadorOrgao = false;	
+		Date dataNascimento = new Date();
+
+		// classe Operadores. java
+		
+		double soma = 10.5 + 15.7;
+		int subtracao = 113 -25;
+		int multiplicacao = 20 * 7;
+		int divisao = 15 / 3;
+		int modulo = 18 % 3;
+		double resultado = (10 * 7) + (20/4);
+		
+		
+		System.out.println("resultado: " + resultado);
+		System.out.println("soma: " + soma);
+		System.out.println("resultado de: " + (resultado + soma));
+		
+		
+		 
+		
+
+	}
+
+}
+
