7538b5c7eb778605253d6c125b82ade27dd022be
Gragolha <luka.100.nerozi@gmail.com>
2023-03-22 19:58:11 -0300

    First commit

---
+++ src/novoTeste/variaveis/Variaveis.java
@@ -0,0 +1,18 @@
+package novoTeste.variaveis;
+
+public class Variaveis {
+	
+	public static void main (String [] args) {
+		
+		//tipos primitivos
+		//Classe String que representa texto na aplicação
+		
+		final int REAL = 10;
+		
+		int dmgFogo = 5;
+		
+		System.out.println(REAL + dmgFogo);
+		
+	}
+
+}
