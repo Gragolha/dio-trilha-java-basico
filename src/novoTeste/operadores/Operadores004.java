37cf4eb93d70da093196c56ee1a6ac751f928c72
Gragolha <luka.100.nerozi@gmail.com>
2023-03-22 22:07:13 -0300

    third commit

---
+++ src/novoTeste/operadores/Operadores004.java
@@ -0,0 +1,28 @@
+package novoTeste.operadores;
+
+public class Operadores004 {
+	
+	public static void main (String [] args) {
+		
+		
+		int numero = 5;
+		
+		//x repeticao
+				
+		++numero;
+		
+		//System.out.println(numero);
+		
+		
+		// boolean
+		
+		
+		boolean variavel = true;
+		
+		variavel = !variavel;
+		
+		System.out.println(variavel);
+		
+	}
+
+}
